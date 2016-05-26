e1d584c0a0580af6cb1ffea98abcf1e39992a8c5
Kyle Winckler <kyle.winckler@gmail.com>
2016-05-25 21:06:14 -0700

    First commit

---
+++ app.js
@@ -0,0 +1,12 @@
+// var userName = prompt('What\'s your name?');
+// alert('Welcome to my page, ' + userName);
+
+
+var questionOne = prompt("What state was I born in?").toLowerCase();
+// questionOne.toLowerCase();
+
+if (questionOne === "south dakota") {
+  alert("Yay! You got it right! \n A+");
+} else {
+  alert("Uh oh, better luck next time!");
+}
